5d6585e0ea527b581b4c61b81855945bddb84b09
Hiich <77889463+Randell-janus@users.noreply.github.com>
2021-03-31 21:39:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = MultiApp()
 st.set_page_config(page_title='Heart Failure EDA')
 st.title('Heart Failure Prediction')
 st.subheader('This app uses the ***Random Forest Classifier***')
-st.write('<== Check sidebar for Directory')
+#st.write('<== Check sidebar for Directory')
 st.write('---')
 st.sidebar.title('Directory')
 
